1a23cc833514b08cf753c8655b243b162bcbfb2f
Alessio <alss.ldt@gmail.comm>
2026-03-04 16:28:52 +0100

    modificato testo cookie banner

--- components/CookieBanner.js
+++ components/CookieBanner.js
@@ -52,7 +52,7 @@ export default function CookieBanner() {
   animate-[slideUp_.25s_ease-out]">
             <p className="text-sm max-w-xl">
                 Questo sito utilizza contenuti di terze parti come Google Maps che
-                potrebbero installare cookie tecnici.
+                utilizzare cookies tecnici.
                 <br />
                 Continuando accetti l'utilizzo di questi servizi.
             </p>
