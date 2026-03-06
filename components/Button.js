7dae3af02718d25d38ce8dbf527ce65f06a679e2
Alessio <alss.ldt@gmail.comm>
2026-03-06 11:50:26 +0100

    aggiunto micro animazioni a bottoni per desktop

--- components/Button.js
+++ components/Button.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
 const variants = {
-  primary: "bg-brand-red text-white hover:bg-brand-redHover",
-  outline: "border border-brand-red text-brand-red hover:bg-brand-red hover:text-brand-white",
-  white: "border border-brand-white text-brand-white hover:bg-brand-white hover:text-brand-red",
+  primary: "bg-brand-red text-white hover:bg-brand-redHover lg:shadow-soft overflow-hidden lg:transition-all duration-300 lg:hover:-translate-y-1 lg:hover:shadow-xl",
+  outline: "border border-brand-red text-brand-red hover:bg-brand-red hover:text-brand-white lg:shadow-soft overflow-hidden lg:transition-all duration-300 lg:hover:-translate-y-1 lg:hover:shadow-xl",
+  white: "border border-brand-white text-brand-white hover:bg-brand-white hover:text-brand-red lg:shadow-soft overflow-hidden lg:transition-all duration-300 lg:hover:-translate-y-1 lg:hover:shadow-xl",
 };
 
 export default function Button({ href, children, variant = "primary", className = "" }) {
